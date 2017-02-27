ee00f46ba56e2fc7c7a6ba584f3d837b3504cef2
ranzhouli <874177815@qq.com>
2017-02-27 22:52:05 +0800

    fm

--- members/冉周礼/FM/js/getmusic.js
+++ members/冉周礼/FM/js/getmusic.js
@@ -1,10 +1,11 @@
 function GetMusic($ct) {
     this.$ct = $ct;
+
     this.init();
     this.bind();
 }
 
-GetMusic.prototype.init = function(){
+GetMusic.prototype.init = function() {
     this.$audio = $('#music');
     this.audio = $('#music')[0];
     this.$panel = this.$ct.find('.panel');
@@ -14,7 +15,7 @@ GetMusic.prototype.init = function(){
     this.$onoff = this.$ct.find('.on-off');
     this.$prev = this.$ct.find('.prev');
     this.$next = this.$ct.find('.next');
-    this.$songName =this.$ct.find('.title .song-name');
+    this.$songName = this.$ct.find('.title .song-name');
     this.$singer = this.$ct.find('.title .singer');
     this.$rotate = this.$ct.find('.rotate');
     this.$disco = this.$ct.find('.rotate .disco');
@@ -25,17 +26,16 @@ GetMusic.prototype.init = function(){
     this.$lyricCt = this.$ct.find('.lyric-ct');
     this.$lyricBox = this.$ct.find('.lyric-box');
     this.$lyricBtn = this.$ct.find('.lyric-btn');
-    this.channelId = 'pubkic_tuijian_spring';
+    this.channelId = 'public_tuijian_spring';
     this.Song = {};
     this.currentTiemSec = 0;
     this.lyricTimeArr = [];
     this.islyricShow = false;
     this.letsPlay = false;
     this.SongArr = [];
-
 }
 
-GetMusic.prototype.bind = function(){
+GetMusic.prototype.bind = function() {
     this.channelsIconChange();
     this.channelSelect();
     this.panelReady();
@@ -48,42 +48,44 @@ GetMusic.prototype.bind = function(){
     this.needleChange();
     this.lyricShow();
     this.timeUpdate();
-};
-// 频道列表切换
-GetMusic.prototype.channelsIconChange = function(){
+}
+
+GetMusic.prototype.channelsIconChange = function() {
     var _this = this;
-    this.$channels.on('mouseover', function(){
+    this.$channels.on('mouseover', function() {
         _this.$channelsList.fadeIn(200);
         _this.$channelBtn.removeClass('icon-menu').addClass('icon-minus');
-    });
-    this.$channels.on('mouseleave', function(){
+    })
+    this.$channels.on('mouseleave', function() {
         _this.$channelsList.fadeOut(200);
         _this.$channelBtn.removeClass('icon-minus').addClass('icon-menu');
     })
-};
+}
 
-GetMusic.prototype.channelSelect = function(){
+GetMusic.prototype.channelSelect = function() {
     var _this = this;
-    this.$channelsList.on('click', 'li', function(){
+    this.$channelsList.on('click', 'li', function() {
         _this.audio.pause();
         $(this).siblings().removeClass('list-selected');
         $(this).addClass('list-selected');
         _this.channelId = $(this).attr('channel-id');
+        // _this.SongArr = [];
         _this.letsPlay = true;
         _this.getAndReset(_this.channelId);
-    });
-};
+    })
+}
 
-GetMusic.prototype.panelReady = function(){
+
+GetMusic.prototype.panelReady = function() {
     var _this = this;
-    this.$ct.ready(function(){
+    this.$ct.ready(function() {
         $.get('http://api.jirengu.com/fm/getChannels.php')
-            .done(function(channelsStr){
-                var channelsArr =JSON.parse(channelsStr).channels;
-                for (var i = 0; i < channelsArr.length; i++){
+            .done(function(channelsStr) {
+                var channelsArr = JSON.parse(channelsStr).channels;
+                for (var i = 0; i < channelsArr.length; i++) {
                     var channelName = channelsArr[i].name;
                     var channelID = channelsArr[i].channel_id;
-                    var html = '<li channel-id=\"' + channelID + '\">' + channelName +'</li>';
+                    var html = '<li channel-id=\"' + channelID + '\" >' + channelName + '</li>';
                     _this.$channelsList.append(html);
                 }
                 $('.channels-list li').first().addClass('list-selected');
@@ -91,24 +93,81 @@ GetMusic.prototype.panelReady = function(){
                 _this.$disco.toggleClass('active');
                 _this.$disco.css('animation-play-state', 'paused');
             })
-    });
+    })
+
 }
-GetMusic.prototype.getAndReset = function(str){
+GetMusic.prototype.rotateCtrl = function() {
+    var _this = this;
+    this.$audio.on('play', function() {
+        _this.$disco.css('animation-play-state', 'running');
+    })
+    this.$audio.on('pause', function() {
+        _this.$disco.css('animation-play-state', 'paused');
+    })
+}
+GetMusic.prototype.canPlay = function() {
+    var _this = this;
+    this.$audio.on('canplay', function() {
+        if (_this.letsPlay) {
+            _this.audio.play();
+        };
+        _this.letsPlay = false;
+    })
+}
+GetMusic.prototype.autoPlay = function() {
+    var _this = this;
+    this.$audio.on('ended', function() {
+        _this.letsPlay = true;
+        _this.getAndReset(_this.channelId);
+    })
+
+}
+
+
+// 上一曲
+GetMusic.prototype.prev = function() {
+    var _this = this;
+    this.$prev.on('click', function() {
+        if (_this.SongArr.length > 1) {
+            _this.SongArr.pop();
+            _this.songReset(_this.SongArr[_this.SongArr.length - 1]);
+            _this.letsPlay = true;
+
+        }
+    })
+    this.$prev.on('mousedown', function() {
+        _this.audio.pause();
+
+    })
+}
+
+
+// 下一曲
+GetMusic.prototype.nextSong = function() {
+    var _this = this;
+    this.$next.on('click', function() {
+        _this.letsPlay = true;
+        _this.getAndReset(_this.channelId);
+    })
+    this.$next.on('mousedown', function() {
+        _this.audio.pause();
+
+    })
+
+}
+
+GetMusic.prototype.getAndReset = function(str) {
     var _this = this;
     $.get('http://api.jirengu.com/fm/getSong.php', {
-        channel: str
-    })
-        .done(function(song){
-
+            channel: str
+        })
+        .done(function(song) {
             _this.Song = JSON.parse(song).song[0];
             _this.songReset(_this.Song);
             _this.SongArr.push(_this.Song);
-
         })
-};
-
-
-GetMusic.prototype.songReset = function(Song){
+}
+GetMusic.prototype.songReset = function(Song) {
     this.audio.src = Song.url;
     this.audio.load();
     this.audio.currentTime = 0;
@@ -116,175 +175,121 @@ GetMusic.prototype.songReset = function(Song){
     this.$songName.text(Song.title);
     this.$singer.text(Song.artist);
     this.$cover.css('background-image', 'url(\'' + Song.picture + '\')');
-};
-
-
-
-GetMusic.prototype.rotateCtrl = function(){
-    var _this = this;
-    this.$audio.on('play', function(){
-        _this.$disco.css('animation-play-state', 'running');
-    });
-    this.$audio.on('pause', function(){
-        _this.$disco.css('animation-play-state', 'paused');
-    });
 }
 
-GetMusic.prototype.canPlay = function(){
-    var _this = this;
-    this.$audio.on('canplay', function(){
-       if(_this.letsPlay){
-           _this.audio.play();
-       }
-        _this.letsPlay = false;
-    });
-};
 
-GetMusic.prototype.autoPlay = function(){
-    var _this = this;
-    this.$audio.on('ended', function(){
-        _this.letsPlay = true;
-        _this.getAndReset(_this.channelId);
-    })
-};
-// 上一曲
-GetMusic.prototype.prev = function(){
-    var _this = this;
-    this.$prev.on('click', function(){
-        if (_this.SongArr.length>1){
-            _this.SongArr.pop();
-            _this.songReset(_this.SongArr[_this.SongArr.length-1]);
-            _this.letsPlay = true;
-        }
-    });
-    this.$prev.on('mousedown', function(){
-        _this.audio.pause();
-    })
 
-};
-GetMusic.prototype.nextSong = function(){
-    var _this = this;
-    this.$next.on('click', function(){
-        _this.letsPlay = true;
-        _this.getAndReset(_this.channelId);
-    });
-    this.$next.on('mousedown', function(){
-        _this.audio.pause();
-    })
-};
 
-// 播放暂停
-GetMusic.prototype.onOff = function(){
+
+// 播放暂停功能
+GetMusic.prototype.onOff = function() {
     var _this = this;
-    this.$onoff.on('click', function(){
-        if(_this.audio.paused){
+    this.$onoff.on('click', function() {
+        if (_this.audio.paused) { // 暂停播放的时候
             _this.audio.play();
-            if(_this.$onoff.hasClass('icon-start1')){
+            if (_this.$onoff.hasClass('icon-start1')) {
                 _this.$onoff.removeClass('icon-start1');
             }
             _this.$onoff.addClass('icon-stop');
-        }else{
+        } else {
             _this.audio.pause();
-            if(_this.$onoff.hasClass('icon-stop')){
+            if (_this.$onoff.hasClass('icon-stop')) {
                 _this.$onoff.removeClass('icon-stop');
             }
             _this.$onoff.addClass('icon-start1');
         }
     })
-    this.$audio.on('play', function(){
-        if(_this.$onoff.hasClass('icon-start1')){
+    this.$audio.on('play', function() {
+        if (_this.$onoff.hasClass('icon-start1')) {
             _this.$onoff.removeClass('icon-start1');
         }
         _this.$onoff.addClass('icon-stop');
     })
-    this.$audio.on('pause', function(){
-        if(_this.$onoff.hasClass('icon-stop')){
+    this.$audio.on('pause', function() {
+        if (_this.$onoff.hasClass('icon-stop')) {
             _this.$onoff.removeClass('icon-stop');
         }
         _this.$onoff.addClass('icon-start1');
     })
-};
+}
 
-// 黑胶指针转动
-GetMusic.prototype.needleChange = function(){
+//  黑胶指针转动
+GetMusic.prototype.needleChange = function() {
     var _this = this;
-    this.$audio.on('play', function(){
+    this.$audio.on('play', function() {
         _this.$needle.addClass('needle-play');
-    });
-    this.$audio.on('pause', function(){
+    })
+    this.$audio.on('pause', function() {
         _this.$needle.removeClass('needle-play');
-    });
-};
+    })
+}
 
-// 歌词滚动
-GetMusic.prototype.lyricShow = function(){
-  var _this = this;
-    this.$lyricBtn.on('click', function(){
-        if(!_this.islyricShow){
+
+
+//  歌词滚动
+GetMusic.prototype.lyricShow = function() {
+    var _this = this;
+    this.$lyricBtn.on('click', function() {
+        if (!_this.islyricShow) {
             _this.$lyricBtn.css('color', '#db4437');
             _this.$lyric.fadeIn(500);
             _this.$rotate.fadeOut(500);
             _this.islyricShow = true;
-        }else{
+        } else {
             _this.$lyricBtn.css('color', 'rgba(170, 170, 170, 0.7)');
             _this.$rotate.fadeIn(500);
             _this.$lyric.fadeOut(500);
             _this.islyricShow = false;
         }
-    });
-};
+    })
+}
 
-GetMusic.prototype.lyricReset = function(sidstr){
+GetMusic.prototype.lyricReset = function(sidstr) {
     var _this = this;
     $.post('http://api.jirengu.com/fm/getLyric.php', {
-        sid: sidstr
-    })
-        .done(function(lyric){
+            sid: sidstr
+        })
+        .done(function(lyric) {
             var Lyric = JSON.parse(lyric).lyric;
             $('.lyric-box>p').remove();
-            _this.lyticTimeArr= [];
+            _this.lyricTimeArr = [];
             _this.lyricFormat(Lyric);
         })
 }
-
-GetMusic.prototype.lyricFormat = function(str){
-  var html = '';
+GetMusic.prototype.lyricFormat = function(str) {
+    var html = '';
     var lyricArr = str.split('\n');
-    var pattern = /^\[\d{2}\:\d{2}\.\d{2}\]/;
-    for(var i= 0; i<lyricArr.length;i++){
-        if(pattern.test(lyricArr[i])){
-            var lyric = lyricArr[i].slice(10, 48);
-
-            if(!lyric){
-                lyric = '-';
-            }
-            // html += '<p class=' + '\"lyric' + i +'\">' + lyric + '</p>';
-            html += '<p class="lyric' + i + '">' + lyric + '</p>';
-            this.lyricTimeFormat(lyricArr[i]);
-        }
+    for (var i = 0; i < lyricArr.length; i++) {
+        var lyric = lyricArr[i].slice(10, 48);
+        if (!lyric) {
+            lyric = '-';
+        };
+        html += '<p class=' + '\"lyric' + i + '\">' + lyric + '</p>';
+        this.lyricTimeFormat(lyricArr[i]);
     }
     this.$lyricBox.append(html);
-};
+}
 
-GetMusic.prototype.lyricTimeFormat = function(str){
-    var min = parseFloat(str.slice(1.3));
-    var sec = Math.round(min * 60 + parseFloat(str.slice(4,9)));
+GetMusic.prototype.lyricTimeFormat = function(str) {
+    var min = parseFloat(str.slice(1, 3));
+    var sec = Math.round(min * 60 + parseFloat(str.slice(4, 9)));
     this.lyricTimeArr.push(sec);
-};
+}
 
-GetMusic.prototype.timeUpdate = function(){
+GetMusic.prototype.timeUpdate = function() {
     var _this = this;
-    this.$audio.on('timeupdate', function(){
-        if(_this.currentTiemSec != Math.round(_this.audio.currentTime)){
+    this.$audio.on('timeupdate', function() {
+        if (_this.currentTiemSec != Math.round(_this.audio.currentTime)) {
             _this.currentTiemSec = Math.round(_this.audio.currentTime);
             _this.lyricBoxMove(_this.currentTiemSec);
         }
-    });
-};
+    })
 
-GetMusic.prototype.lyricBoxMove = function(num){
-    for(var i = 0; i < this.lyricTimeArr.length; i++){
-        if(num === this.lyricTimeArr[i]){
+}
+
+GetMusic.prototype.lyricBoxMove = function(num) {
+    for (var i = 1; i < this.lyricTimeArr.length; i++) {
+        if (num === this.lyricTimeArr[i]) {
             var Top = 80 - i * 40 + 'px';
             var lightClass = '.lyric' + i;
             $(lightClass).siblings().removeClass('light-lyric');
@@ -294,4 +299,4 @@ GetMusic.prototype.lyricBoxMove = function(num){
             }, 300);
         }
     }
-};
+}
--- members/冉周礼/FM/js/progress.js
+++ members/冉周礼/FM/js/progress.js
@@ -1,13 +1,15 @@
-/**
- * Created by Rana on 2017/2/25.
- */
-function Progress($ct){
+function Progress($ct) {
+
     this.$ct = $ct;
+
     this.init();
     this.bind();
-};
+}
 
-Progress.prototype.init = function(){
+
+
+
+Progress.prototype.init = function() {
     this.$audio = $('#music');
     this.audio = $('#music')[0];
     this.$FM = $('#fm');
@@ -19,88 +21,103 @@ Progress.prototype.init = function(){
     this.$currentTime = this.$ct.find('.current-time');
     this.$fullTime = this.$ct.find('.full-time');
     this.clock1;
-    
-    
-    //拖拽可控制播放进度
+
+    // 拖拽可控制播放进度
     this.drag = this.$progressHandle.draggabilly({
         axis: 'x',
         containment: true
     })
 }
 
-Progress.prototype.bind = function(){
+
+
+
+
+Progress.prototype.bind = function() {
     this.dragMove();
     this.clickCtrl();
     this.timeText();
+    // this.pauseClearInterval();
 }
 
-Progress.prototype.dragMove = function(){
+
+Progress.prototype.dragMove = function() {
     var _this = this;
-    this.drag.on('dragMove', function(){
+    this.drag.on('dragMove', function() {
         var draggie = $(this).data('draggabilly');
         var width = draggie.position.x + 'px';
         _this.$progressLine.css('width', width);
-    });
-    this.drag.on('dragStart', function(){
-       _this.audio.pause();
     })
-    this.drag.on('dragEnd', function(){
+    this.drag.on('dragStart', function() {
+        _this.audio.pause();
+    })
+    this.drag.on('dragEnd', function() {
         _this.audio.play();
         var draggie = $(this).data('draggabilly');
         _this.audio.currentTime = draggie.position.x / 200 * _this.audio.duration;
-    });
+    })
 }
+
+
+
 // 点击控制进度
-Progress.prototype.clickCtrl = function(){
+Progress.prototype.clickCtrl = function() {
     var _this = this;
-    this.$progressBar.on('click', function(e){
+    this.$progressBar.on('click', function(e) {
         var clickX = e.clientX;
-        var barLeft;
-        barLeft = _this.toLength(_this.$progressBar.css('left'))
-            + _this.toLength(_this.$progress.css('left'))
-            + _this.toLength(_this.$FM.css('left'));
+        var barLeft = _this.toLength(_this.$progressBar.css('left'))
+                + _this.toLength(_this.$progress.css('left'))
+                + _this.toLength(_this.$FM.css('left'));
         var left = clickX - barLeft - 4;
         _this.$progressLine.width(left);
         _this.$progressHandle.css('left', left);
-        _this.audio.currentTime = left/200 * _this.audio.duration;
-    });
-};
+        _this.audio.currentTime = left / 200 * _this.audio.duration;
+    })
+}
 
-Progress.prototype.toLength = function(str){
+Progress.prototype.toLength = function(str) {
     var num = parseInt(str.replace('px', ''));
-    return num;
-};
+    return num
+}
 
 // 显示播放时长及进度
-Progress.prototype.timeText = function(){
+
+Progress.prototype.timeText = function() {
     var _this = this;
-    this.$audio.on('play', function(){
+    this.$audio.on('play', function() {
         var fullTime = _this.audio.duration;
-        _this.clock1 = setInterval(function(){
+        _this.clock1 = setInterval(function() {
             var currentTime = _this.audio.currentTime;
-            console.log(_this.audio.currentTime)
+            console.log(currentTime)
             var currentWidth = parseInt(currentTime / fullTime * 200) + 'px';
             _this.$currentTime.text(_this.timeFormat(currentTime));
             _this.$progressLine.width(currentWidth);
-            _this.$progressHandle.css('left', currentWidth);
-
+            _this.$progressHandle.css('left', currentWidth)
         }, 1000)
-
         _this.$fullTime.text(_this.timeFormat(fullTime));
     });
-};
+    this.$audio.on('pause', function() {
+        clearInterval(_this.clock1);
+    })
+}
 
+// Progress.prototype.pauseClearInterval = function() {
+//     var _this = this;
+//     this.$audio.on('pause', function() {
+//         console.log(1);
+//         clearInterval(0);
+//     })
+// }
 
-Progress.prototype.timeFormat = function(num){
+Progress.prototype.timeFormat = function(num) {
     var fullSec = parseInt(num);
-
     var min = parseInt(fullSec / 60) + '';
     var sec = (fullSec % 60);
-    if(sec < 10){
+    if (sec < 10) {
         sec = '0' + sec;
-    }else{
+    } else {
         sec = sec + '';
     }
     var timeStr = min + ':' + sec;
     return timeStr;
-};
+}
