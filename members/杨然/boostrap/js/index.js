96d0fc248ca9e3d98b02e2af82f6912ce554359c
yangran <sfyr111@163.com>
2017-03-08 23:32:51 +0800

    huy

--- members/杨然/boostrap/js/index.js
+++ members/杨然/boostrap/js/index.js
@@ -1,4 +1,4 @@
-$(function(){
+// $(function(){
 	//当文档加载完成才会执行ready()
 
 	$(window).on('resize', resize).trigger('resize');//trigger让window对象立即出发resize事件
@@ -21,6 +21,48 @@ $(function(){
 				$node.empty();
 			}
 		});
+
+		// 控制标签页tab的容器宽度
+		var $ulCon = $('ul.nav-tabs');
+		var width = 0;
+		$ulCon.children().each(function(index, node) {
+			var $node = $(node);
+			width += $node.width();
+
+		});
+		// if tab 宽度超出屏幕宽度才显示滚动条
+		if (width > $(window).width()) {
+			$ulCon.width(width);
+		} else {
+			$ulCon.width('100%');
+		}
 	}
 
-})
+	var $newsTitle = $('.news-title');
+	$('#news .nav-pills a').on('click', function(){
+		var $this = $(this);
+		var title = $this.data('title');
+		$newsTitle.text(title);
+	})
+
+	// 轮播图滑动操作
+	var $carousels = $('.carousel');
+	var startX;
+	var endX;
+	$carousels.on('touchstart', function(e){
+		startX = e.originalEvent.touches[0].clientX
+		// console.log(startX);
+	});
+	$carousels.on('touchmove', function(e){
+		endX = e.originalEvent.touches[0].clientX
+		// console.log(endX);
+	});
+	$carousels.on('touchend', function(){
+		if ((startX - endX) > 60) {
+			$carousels.carousel('next');
+		} else if ((endX - startX) > 60) {
+			$carousels.carousel('prev');
+		}
+	})
+	
+// })
